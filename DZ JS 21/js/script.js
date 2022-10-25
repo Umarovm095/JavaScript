2804c9ea18a7a10dd83da6d7b38680b8b7fb3a86
EgzeM <114302378+Umarovm095@users.noreply.github.com>
2022-10-25 14:01:01 +0300

    Update script.js

--- DZ JS 21/js/script.js
+++ DZ JS 21/js/script.js
@@ -1,3 +1,33 @@
-let arr1 = [101, 202, 303, 404, 505]
+let arr_1 = [111, 222, 333, 444, 555];
+let arr_2 = [606, 707, 808, 909];
 
-let arr2 = [606, 707, 808, 909]
+let superArr = [...arr_1, ...arr_2];
+
+console.log(superArr);
+
+console.log(...superArr);
+
+console.log(Math.min(...superArr));
+
+
+
+
+let obj = {
+    width: 400,
+    height: 700,
+};
+
+let obj2 = {...obj, area: function(){
+    return obj2.width + obj2.height
+    },
+};
+console.log(obj2);
+console.log(obj2.area);
+
+
+
+
+
+for (let element = 1; element < 11; element++){
+    console.log(element);
+}
